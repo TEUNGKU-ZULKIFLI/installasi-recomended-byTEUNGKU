ae8ecb0095d7a0e0722837997e7d71f56bcfc8e6
TEUNGKU ZULKIFLI <120408238+TEUNGKU-ZULKIFLI@users.noreply.github.com>
2024-08-23 10:54:18 +0000

    LATIHAN 004 : PERULANGAN IF ELSE pada Angkot

--- LATIHAN/angkot1.js
+++ LATIHAN/angkot1.js
@@ -1,12 +1,9 @@
 var jmlAngkot = 10;
 var angkotBeroperasi = 6;
-var noAngkot = 1;
-
-while (noAngkot <= angkotBeroperasi) {
-    console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.');
-    noAngkot++;
-}
-
-for (noAngkot = angkotBeroperasi + 1; noAngkot <= jmlAngkot; noAngkot++) {
-    console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi.');
+for (var noAngkot = 1; noAngkot <= jmlAngkot; noAngkot++) {
+    if (noAngkot <= angkotBeroperasi) {
+        console.log('Angkot No. ' + noAngkot + ' beroperasi dengan baik.');
+    } else {
+        console.log('Angkot No. ' + noAngkot + ' sedang tidak beroperasi.');
+    }
 }
