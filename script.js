9c8bf2054000ebe65891cec59cf14fa50e254cce
TEUNGKU ZULKIFLI <120408238+TEUNGKU-ZULKIFLI@users.noreply.github.com>
2024-08-23 09:25:55 +0000

    LANGKAH 003 : POPUP BOX [KOMBINASI SEMUANYA]

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
-var tes = confirm ('Kamu yakin ?');
-if (tes === true ) {
-    alert ('User menekan OK!');
-}else {
-    alert ('User menekan Cancel');
+alert ('SELAMAT DATANG !');
+var lagi = true;
+
+while (lagi) {  // bisa juga ditulis: while (lagi == true)
+    var nama = prompt('Masukkan nama: ');
+    alert('Halo ' + nama);
+
+    lagi = confirm('Coba lagi ?');
 }
+
+alert('Terima kasih…');
